930d224660a87ea31f2e8c6b6a85227fc01226e7
e2400501 <enesyucel38@icloud.com>
2025-04-30 16:33:42 +0200

    n

--- TPII/firstapp/urls.py
+++ TPII/firstapp/urls.py
@@ -3,4 +3,8 @@ from . import views
 
 urlpatterns = [
     path('ajout/', views.ajout),
+    path('traitement/', views.traitement),
+    path('/affiche/<int:id>/',views.read),
+    path('VUE_Affiche_all/', views.VUE_Affiche_all),
+    path('/update/<int:id>/',views.update),
 ]
